231dadd8923ca0e23e0172db7f8c38f7555ef8d1
Aleksey Ilyin <alksily@outlook.com>
2023-01-20 17:41:25 +0200

    add TranslateLetters

--- LangRussian/LangRussianPlugin.php
+++ LangRussian/LangRussianPlugin.php
@@ -13,12 +13,27 @@ class LangRussianPlugin extends AbstractPlugin
     const TITLE = 'Русский язык';
     const AUTHOR = 'Aleksey Ilyin';
     const AUTHOR_SITE = 'https://getwebspace.org';
-    const VERSION = '1.1';
+    const VERSION = '1.1.1';
 
     public function __construct(ContainerInterface $container)
     {
         parent::__construct($container);
 
         $this->addLocaleFromFile('ru-RU', __DIR__ . '/Locale/_.php');
+
+        $this->addLocaleTranslateLetters('ru-RU',
+            [
+                'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У',
+                'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
+                'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь',
+                'э', 'ю', 'я',
+            ],
+            [
+                'A', 'B', 'V', 'G', 'D', 'E', 'E', 'Zh', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U',
+                'F', 'Kh', 'C', 'Ch', 'Sh', 'Sch', '', 'Y', '', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'zh',
+                'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'kh', 'c', 'ch', 'sh', 'sch', '',
+                'y', '', 'e', 'yu', 'ya',
+            ]
+        );
     }
 }
